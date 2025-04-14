58c0713f5d7ae2480e68bab74c5b90a61aa7a3d0
Cava <89451089+AchievementCava@users.noreply.github.com>
2025-04-15 00:21:22 +0100

    More cosmetic changes, app should be ready for use

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -29,12 +29,7 @@
 </svelte:head>
 
 <Panel title="There are no upcoming elections">
-	<p>Check this space later for updates. Here's a random TeX for now.</p>
-	<img
-		src={`https://cssuob.github.io/resources/dinosaur/tex_${image}.svg`}
-		alt="A variant of TeX, the mascot of CSS"
-		height="100px"
-	/>
+	<p>Check this space later for updates.</p>
 </Panel>
 
 <style>
